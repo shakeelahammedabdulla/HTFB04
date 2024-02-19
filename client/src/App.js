10f3828db7bbe5fa340a9e9268679243540e1a45
shakeelahammedabdulla <www.codewithbro@gmail.com>
2024-02-20 03:12:29 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -24,78 +24,156 @@
 
 // export default App;
 
+///////////////////////////////////////////////////////////////////////
+
+//reference code design
+
+// import { useEffect, useState } from "react";
+// import { ethers } from "ethers";
+// import Upload from "./artifacts/contracts/Upload.sol/Upload.json";
+// import "./App.css";
+// import FileUpload from "./components/FileUpload";
+// import Display from "./components/Display";
+// import Modal from "./components/Modal";
+
+
+// function App() {
+//   const [account, setAccount] = useState("");
+//   const [contract, setContract] = useState(null);
+//   const [provider, setProvider] = useState(null);
+//   const [modelOpen, setModelOpen] = useState(false);
+
+//   useEffect(() => {
+//     const wallet = async () => {
+//       if (provider) {
+//         await provider.send("eth_requestAccounts", []);
+//         const signer = provider.getSigner();
+//         const address = await signer.getAddress();
+//         console.log(address);
+//         setAccount(address);
+
+//         const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+//         // const contractAddress = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+
+//         // 0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0
+//         const contract = new ethers.Contract(contractAddress, Upload.abi, signer);
+//         console.log(contract);
+//         setContract(contract);
+//         setProvider(signer);
+//       } else {
+//         alert("Metamask is not installed");
+//       }
+//     };
+//     provider && wallet();
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Secure File Upload</h1>
+//         <div className="Account-info">
+//           <h2>Connected Account:</h2>
+//           <p>{account}</p>
+//         </div>
+//         <button className="Upload-button" onClick={() => setModelOpen(true)}>
+//           Upload File
+//         </button>
+//       </header>
+//       {modelOpen && (
+//         <div className="Modal">
+//           <div className="Modal-content">
+//             <h3>Upload File</h3>
+//             <input type="file" />
+//             <button className="Close-button" onClick={() => setModelOpen(false)}>
+//               Close
+//             </button>
+//             <button className="Submit-button">Submit</button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
 import Upload from "./artifacts/contracts/Upload.sol/Upload.json";
-import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import FileUpload from "./components/FileUpload";
 import Display from "./components/Display";
 import Modal from "./components/Modal";
-
+import "./App.css";
 
 function App() {
   const [account, setAccount] = useState("");
   const [contract, setContract] = useState(null);
   const [provider, setProvider] = useState(null);
-  const [modelOpen, setModelOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
-    const wallet = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async () => {
       if (provider) {
+        window.ethereum.on("chainChanged", () => {
+          window.location.reload();
+        });
+
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
-        console.log(address);
         setAccount(address);
+        let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-        // const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-        const contractAddress = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
-
-        // 0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0
-        const contract = new ethers.Contract(contractAddress, Upload.abi, signer);
-        console.log(contract);
+        const contract = new ethers.Contract(
+          contractAddress,
+          Upload.abi,
+          signer
+        );
+        //console.log(contract);
         setContract(contract);
-        setProvider(signer);
+        setProvider(provider);
       } else {
-        alert("Metamask is not installed");
+        console.error("Metamask is not installed");
       }
     };
-    provider && wallet();
+    provider && loadProvider();
   }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Secure File Upload</h1>
-        <div className="Account-info">
-          <h2>Connected Account:</h2>
-          <p>{account}</p>
-        </div>
-        <button className="Upload-button" onClick={() => setModelOpen(true)}>
-          Upload File
+    <>
+      {!modalOpen && (
+        <button className="share" onClick={() => setModalOpen(true)}>
+          Share
         </button>
-      </header>
-      {modelOpen && (
-        <div className="Modal">
-          <div className="Modal-content">
-            <h3>Upload File</h3>
-            <input type="file" />
-            <button className="Close-button" onClick={() => setModelOpen(false)}>
-              Close
-            </button>
-            <button className="Submit-button">Submit</button>
-          </div>
-        </div>
       )}
-    </div>
+      {modalOpen && (
+        <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
+      )}
+
+      <div className="App">
+        <h1 style={{ color: "white" }}>Gdrive 3.0</h1>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+
+        <p style={{ color: "white" }}>
+          Account : {account ? account : "Not connected"}
+        </p>
+        <FileUpload
+          account={account}
+          provider={provider}
+          contract={contract}
+        ></FileUpload>
+        <Display contract={contract} account={account}></Display>
+      </div>
+    </>
   );
 }
 
 export default App;
-
-
-
-//0x5fbdb2315678afecb367f032d93f642f64180aa3
